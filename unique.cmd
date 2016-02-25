f173d4a50663aba0e79095d0f7b63cd9ab64d78f
Kaz Gofron <kgofron@comcast.net>
2016-02-24 21:19:45 -0500

    change prefix to allow timeSeries measurements

--- unique.cmd
+++ unique.cmd
@@ -7,7 +7,7 @@
 #epicsEnvSet("TSPOINTS",  "1000")
 #epicsEnvSet("IP",        "164.54.160.241:10001")
 
-epicsEnvSet("PREFIX",    "XF:10ID-BI:")
+epicsEnvSet("PREFIX",    "XF10ID-BI:")
 epicsEnvSet("RECORD",    "TM176")
 epicsEnvSet("PORT",      "TM176")
 epicsEnvSet("TEMPLATE",  "TetrAMM")
@@ -20,6 +20,8 @@ epicsEnvSet("QUAD_DET",        "TetrAMM.cmd")
 
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", "NO")
 epicsEnvSet("EPICS_CA_ADDR_LIST", "10.10.0.255")
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "1000000")
 
 epicsEnvSet("ENGINEER",  "kgofron x5283")
 epicsEnvSet("LOCATION",  "740 IXS beamline")
